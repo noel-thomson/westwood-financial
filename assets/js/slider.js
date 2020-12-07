c1c8fc2e8354282c591a09d41da304cba9b3051e
Noel Thomson <noel.thomson@gmail.com>
2020-12-07 10:38:57 -0500

    production

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -6,13 +6,18 @@ let slideInterval;
 const headers = document.querySelectorAll('.slide-content h1');
 let count = 0;
 
+headers[0].classList.add('active');
+let height = headers[0].clientHeight + 10;
+headers[0].style.top = `-${height}px`;
+
 const slideNext = () => {
   let active = headers[count];
   active.classList.remove('active');
   count >= headers.length - 1 ? (count = 0) : count++;
   active = headers[count];
+  let height = active.clientHeight + 10;
   active.classList.add('active');
-  current.classList.remove('active');
+  active.style.top = `-${height}px`;
 };
 
 if (auto) {
