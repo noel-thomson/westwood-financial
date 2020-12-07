6013f5b510eab3c7b7b968c9854c67ca7d4eb2f3
Noel Thomson <noel.thomson@gmail.com>
2020-12-06 22:57:54 -0500

    working copy

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,18 +2,33 @@ const slides = document.querySelectorAll('.slide');
 const auto = true;
 const intervalTime = 5000;
 let slideInterval;
+//
+const headers = document.querySelectorAll('.slide-content h1');
+let count = 0;
 
 const slideNext = () => {
-  const current = document.querySelector('.current');
-  current.classList.remove('current');
-  if (current.nextElementSibling) {
-    current.nextElementSibling.classList.add('current');
-  } else {
-    slides[0].classList.add('current');
-  }
-  current.classList.remove('current');
+  let active = headers[count];
+  active.classList.remove('active');
+  count >= headers.length - 1 ? (count = 0) : count++;
+  active = headers[count];
+  active.classList.add('active');
+  current.classList.remove('active');
 };
 
 if (auto) {
   slideInterval = setInterval(slideNext, intervalTime);
 }
+// const slideNext = () => {
+//   const current = document.querySelector('.current');
+//   current.classList.remove('current');
+//   if (current.nextElementSibling) {
+//     current.nextElementSibling.classList.add('current');
+//   } else {
+//     slides[0].classList.add('current');
+//   }
+//   current.classList.remove('current');
+// };
+
+// if (auto) {
+//   slideInterval = setInterval(slideNext, intervalTime);
+// }
